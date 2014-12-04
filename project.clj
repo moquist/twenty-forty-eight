89d48ce7de6f704da40000126c315f61bce45b71
Matt Oquist <moquist@majen.net>
2014-12-04 12:25:40 -0500

    put in a real description

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject twenty-forty-eight "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "This is a Clojure implementation of the game 2048."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0-alpha1"]
